d136758259b95cf4cafc48114f26b10d548460fd
Vladislav Ezhkov <vlad.ezhkov.96@mail.ru>
2022-06-22 13:46:06 +0300

    Initial commit

---
+++ src/test/java/RegistrationUserData.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class RegistrationUserData {
+    private final String login;
+    private final String password;
+    private final String status;
+}
